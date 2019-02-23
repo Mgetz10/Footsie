0c90621d347c3cb74f84047a183b7adee649198b
Monroe <mgetz10@gmail.com>
2019-02-23 16:51:27 -0500

    first commit

---
+++ routes/auth-routes.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const authRoutes = express.Router();
+const passport = require('passport');
+const ensureLogin = require('connect-ensure-login');
+
+// User model
+const User = require('../models/user');
+
+// Bcrypt to encrypt passwords
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+
+authRoutes.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+
+authRoutes.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === '' || password === '') {
+    res.render('auth/signup', { message: 'Indicate username and password' });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render('auth/signup', { message: 'The username already exists' });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save(err => {
+        if (err) {
+          res.render('auth/signup', { message: 'Something went wrong' });
+        } else {
+          res.redirect('/private-page');
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login', { message: req.flash('error') });
+});
+
+authRoutes.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/private-page',
+    failureRedirect: '/login',
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+authRoutes.get('/private-page', isLoggedIn, (req, res) => {
+  res.render('private', { user: req.user });
+});
+
+function isLoggedIn(req, res, next) {
+  console.log('islgoedin', req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+authRoutes.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+module.exports = authRoutes;
